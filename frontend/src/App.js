c41ca9f4f8b1dfcf60b1991993868772b0b0ba2f
Zeal5 <tassadiqshakeel5@gmail.com>
2023-07-01 10:43:06 +0500

    start frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+export default function App() {
+
+    return (
+        <div className='app'>
+            <h1>Welcome to My React App</h1>
+        </div>
+
+    );
+
+
+}
